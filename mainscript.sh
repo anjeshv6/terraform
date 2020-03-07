9712069d491222e725d2c56770d44313a93c739b
anjeshv6 <58216907+anjeshv6@users.noreply.github.com>
2020-03-07 14:55:06 +0530

    Update mainscript.sh

--- mainscript.sh
+++ mainscript.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-for i in {1..5};
-do 
-  echo "anjesh kumar" >> ccc.txt
-  echo "================" ccc.txt
+for i in {1..3};
+do
+  echo " Result # $i :" >> results.txt
+  #python gasrun.py >> results.txt
+  ls >> results.txt
+  date  >> results.txt
+  echo "====================================================================" >> results.txt
   sleep 10;
-  git add ccc.txt
-  git commit -m "sample gjfvjg"
-  git push origin master
+  git add ccc.txt >> ccc.txt
+  git commit -m "sample gjfvjg" >> ccc.txt
+  git push origin master >> ccc.txt
 done

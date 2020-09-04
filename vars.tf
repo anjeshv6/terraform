d86b96bd6867502aa084972f6dbe69898a6a2ad0
anjeshv6 <58216907+anjeshv6@users.noreply.github.com>
2020-09-04 12:26:14 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,29 @@
+variable "ec2_count" {
+  default = "1"
+}
+
+variable "ami_id" {
+  default = "ami-0a74bfeb190bd404f"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+
+
+variable "ec2_name" {
+  default = "test-terraform"
+}
+
+variable "key_id" {
+  default = "anjesh321"
+}
+
+variable "region_id" {
+  default = "ap-south-1"
+}
+
+variable "avail_zone" {
+  default = "ap-south-1b"
+}
